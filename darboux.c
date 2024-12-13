88c641859c541de2a86e4f63de4f10bf2137d14b
MedChahb <chahbaoui.mohamed@gmail.com>
2024-12-13 18:21:21 +0100

    good

--- darboux.c
+++ darboux.c
@@ -30,12 +30,10 @@ struct ThreadTiming {
 float max_terrain(const mnt *restrict m)
 {
   float max = m->terrain[0];
-  for (int i = 0; i < m->ncols * m->nrows; i++) {
-    if (m->terrain[i] > max) {
+  for(int i = 0 ; i < m->ncols * m->nrows ; i++)
+    if(m->terrain[i] > max)
       max = m->terrain[i];
-    }
-  }
-  return max;
+  return(max);
 }
 
 // initialise le tableau W de départ à partir d'un mnt m
@@ -47,30 +45,29 @@ float *init_W(const mnt *restrict m)
 
   // initialisation W
   const float max = max_terrain(m) + 10.;
-  for (int i = 0; i < nrows; i++) 
+  for(int i = 0 ; i < nrows ; i++)
   {
-    for (int j = 0; j < ncols; j++) 
+    for(int j = 0 ; j < ncols ; j++)
     {
-      if (i == 0 || i == nrows - 1 || j == 0 || j == ncols - 1 || TERRAIN(m, i, j) == m->no_data)
-        WTERRAIN(W, i, j) = TERRAIN(m, i, j);
+      if(i==0 || i==nrows-1 || j==0 || j==ncols-1 || TERRAIN(m,i,j) == m->no_data)
+        WTERRAIN(W,i,j) = TERRAIN(m,i,j);
       else
-        WTERRAIN(W, i, j) = max;
+        WTERRAIN(W,i,j) = max;
     }
   }
 
-  return W;
+  return(W);
 }
 
 // variables globales pour l'affichage de la progression
 #ifdef DARBOUX_PPRINT
-float min_darboux = 9999.; // ça ira bien, c'est juste de l'affichage
-int iter_darboux = 0;
+float min_darboux=9999.; // ça ira bien, c'est juste de l'affichage
+int iter_darboux=0;
 int printed = 0; // Avec MPI, on print plusieurs newLines indésirables
-
 // fonction d'affichage de la progression
 void dpprint()
 {
-    if (min_darboux != 9999.) 
+    if(min_darboux != 9999.) 
     {
         fprintf(stderr, "%.3f %d\r", min_darboux, iter_darboux++);
         fflush(stderr);
@@ -86,7 +83,7 @@ void dpprint()
 #endif
 
 // pour parcourir les 8 voisins :
-const int VOISINS[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+const int VOISINS[8][2] = {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
 
 // cette fonction calcule le nouveau W[i,j] en utilisant Wprec[i,j]
 // et ses 8 cases voisines : Wprec[i +/- 1 , j +/- 1],
@@ -98,50 +95,59 @@ int calcul_Wij(float *restrict W, const float *restrict Wprec, const mnt *m, con
   int modif = 0;
 
   // on prend la valeur précédente...
-  WTERRAIN(W, i, j) = WTERRAIN(Wprec, i, j);
+  WTERRAIN(W,i,j) = WTERRAIN(Wprec,i,j);
   // ... sauf si :
-  if (WTERRAIN(Wprec, i, j) > TERRAIN(m, i, j)) {
+  if(WTERRAIN(Wprec,i,j) > TERRAIN(m,i,j))
+  {
     // parcourir les 8 voisins haut/bas + gauche/droite
-    for (int v = 0; v < 8; v++) {
+    for(int v=0; v<8; v++)
+    {
       const int n1 = i + VOISINS[v][0];
       const int n2 = j + VOISINS[v][1];
 
       // vérifie qu'on ne sort pas de la grille.
       // ceci est théoriquement impossible, si les bords de la matrice Wprec
       // sont bien initialisés avec les valeurs des bords du mnt
-      CHECK(n1 >= 0 && n1 < nrows && n2 >= 0 && n2 < ncols);
+      CHECK(n1>=0 && n1<nrows && n2>=0 && n2<ncols);
 
       // si le voisin est inconnu, on l'ignore et passe au suivant
-      if (WTERRAIN(Wprec, n1, n2) == m->no_data)
+      if(WTERRAIN(Wprec,n1,n2) == m->no_data)
         continue;
 
-      CHECK(TERRAIN(m, i, j) > m->no_data);
-      CHECK(WTERRAIN(Wprec, i, j) > m->no_data);
-      CHECK(WTERRAIN(Wprec, n1, n2) > m->no_data);
+      CHECK(TERRAIN(m,i,j)>m->no_data);
+      CHECK(WTERRAIN(Wprec,i,j)>m->no_data);
+      CHECK(WTERRAIN(Wprec,n1,n2)>m->no_data);
 
       // il est important de mettre cette valeur dans un temporaire, sinon le
       // compilo fait des arrondis flotants divergents dans les tests ci-dessous
-      const float Wn = WTERRAIN(Wprec, n1, n2) + EPSILON;
-      if (TERRAIN(m, i, j) >= Wn) {
-        WTERRAIN(W, i, j) = TERRAIN(m, i, j);
+      const float Wn = WTERRAIN(Wprec,n1,n2) + EPSILON;
+      if(TERRAIN(m,i,j) >= Wn)
+      {
+        WTERRAIN(W,i,j) = TERRAIN(m,i,j);
         modif = 1;
         #ifdef DARBOUX_PPRINT
-        if (WTERRAIN(W, i, j) < min_darboux)
-          min_darboux = WTERRAIN(W, i, j);
+        if(WTERRAIN(W,i,j)<min_darboux)
+          min_darboux = WTERRAIN(W,i,j);
         #endif
-      } else if (WTERRAIN(Wprec, i, j) > Wn) {
-        WTERRAIN(W, i, j) = Wn;
+      }
+      else if(WTERRAIN(Wprec,i,j) > Wn)
+      {
+        WTERRAIN(W,i,j) = Wn;
         modif = 1;
         #ifdef DARBOUX_PPRINT
-        if (WTERRAIN(W, i, j) < min_darboux)
-          min_darboux = WTERRAIN(W, i, j);
+        if(WTERRAIN(W,i,j)<min_darboux)
+          min_darboux = WTERRAIN(W,i,j);
         #endif
       }
     }
   }
-  return modif;
+  return(modif);
 }
 
+/*****************************************************************************/
+/*           Fonction de calcul principale - À PARALLÉLISER                  */
+/*****************************************************************************/
+// applique l'algorithme de Darboux sur le MNT m, pour calculer un nouveau MNT
 #ifdef MPI
 void exchange_boundaries(float *W, int start, int end, int ncols, int rank, int size)
 {
@@ -223,7 +229,7 @@ mnt *darboux(const mnt *restrict m)
   #endif
 
   while (modif) {
-      modif = 0;
+      modif = 0;  // sera mis à 1 s'il y a une modification
 
       #if defined(OMP) || defined(MPI)
       int tid;
@@ -234,8 +240,11 @@ mnt *darboux(const mnt *restrict m)
           thread_start = omp_get_wtime();
 
           #pragma omp for reduction(|:modif) schedule(dynamic)
+          // calcule le nouveau W fonction de l'ancien (Wprec) en chaque point [i,j]
           for (int i = start; i < end; i++) {
-              for (int j = 0; j < ncols; j++) {
+              for (int j=0; j<ncols; j++){
+                // calcule la nouvelle valeur de W[i,j]
+                // en utilisant les 8 voisins de la position [i,j] du tableau Wprec
                   modif |= calcul_Wij(W, Wprec, m, i, j);
               }
           }
@@ -263,6 +272,8 @@ mnt *darboux(const mnt *restrict m)
       dpprint();
     #endif
 
+    // échange W et Wprec
+    // sans faire de copie mémoire : échange les pointeurs sur les deux tableaux
     float *tmp = W;
     W = Wprec;
     Wprec = tmp;
@@ -273,6 +284,7 @@ mnt *darboux(const mnt *restrict m)
     modif |= global_modif;
     #endif
   }
+  // fin du while principal
 
   #ifdef MPI
   gather_data(W, blockSize, ncols, nrows, rank, size);
@@ -290,11 +302,12 @@ mnt *darboux(const mnt *restrict m)
   }
   free(thread_timings);
   #endif
-
+  // fin du calcul, le résultat se trouve dans W
   free(Wprec);
+  // crée la structure résultat et la renvoie
   mnt *res;
-  CHECK((res = malloc(sizeof(*res))) != NULL);
+  CHECK((res=malloc(sizeof(*res))) != NULL);
   memcpy(res, m, sizeof(*res));
   res->terrain = W;
-  return res;
+  return(res);
 }
